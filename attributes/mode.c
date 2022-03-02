f5ee6b42d370cf46e7133f11da829c916cadb656
Raykuzu <46544369+Raykuzu@users.noreply.github.com>
2022-03-02 19:18:28 +0100

    syntax

--- attributes/mode.c
+++ attributes/mode.c
@@ -2,7 +2,7 @@
 // Created by raykuzu on 19/03/2020.
 // Targets: variables
 // Effects: special needs
-// Force compiler to use a certain size for a variable (integer or float) instead of use system size
+// Force compiler to use a certain size for a variable (integer or float) instead of using system size
 // Possible values for integers are QI (1 byte), HI (2 bytes), SI (4 bytes), DI (8 bytes), TI (16 bytes)
 // Possible values for floats are SF (4 bytes), DF (8 bytes), TF (16 bytes)
 //
